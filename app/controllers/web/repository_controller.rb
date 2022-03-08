26630f7fdc6de3d364f48c8f26d31221e3fc0b25
NoClaus <support@marketer.consulting>
2022-03-08 12:54:43 +0000

    Installed simple form

--- app/controllers/web/repository_controller.rb
+++ app/controllers/web/repository_controller.rb
@@ -1,4 +1,36 @@
 # frozen_string_literal: true
 
-class Web::RepositoryController < ApplicationController
+class Web::RepositoriesController < ApplicationController
+  def index
+    @repositories = current_user.repositories
+  end
+
+  def new
+    available_languages = Repository.language.values
+
+    client = Octokit::Client.new access_token: current_user.token, per_page: 200
+    @repos = client.repos
+                   .select { |repo| repo.language.present? }
+                   .filter { |repo| available_languages.include? repo.language.downcase }
+                   .map { |repo| [repo.full_name, repo.id] }
+
+    @repository = current_user.repositories.build
+  end
+
+  def create
+    @repository = current_user.repositories.build(repository_params)
+
+    if @repository.save
+      RepositoryLoaderJob.perform_later @repository.id, current_user.token
+      redirect_to repositories_path, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def repository_params
+    params.require(:repository).permit(:github_id)
+  end
 end
