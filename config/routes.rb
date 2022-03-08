50b348c5dc07cf86d80d1fdfb160b0907f4d96f0
NoClaus <support@marketer.consulting>
2022-03-08 09:09:15 +0000

    Step 2: Added Welcome controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   scope module: :web do
+    root 'welcome#index'
+    
     get '/auth/:provider/callback', to: 'auth#callback', as: :callback_auth, via: :all
     post '/auth/:provider', to: 'auth#request', as: :auth_request
     delete '/auth/logout', to: 'auth#logout', as: :auth_logout
