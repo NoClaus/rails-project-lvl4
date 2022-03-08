587e95bb4e1beb38f43e8b39892c17116224a19f
NoClaus <support@marketer.consulting>
2022-03-08 11:21:07 +0000

    Rubocop fix

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Repository < ApplicationRecord
   extend Enumerize
-  
+
   belongs_to :user
-  
+
   validates :github_id, presence: true
-  
+
   enumerize :language, in: AVAILABLE_LANGUAGES
 end
