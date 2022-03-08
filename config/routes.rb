33487c79e18265779eacc4a173bfa32c67c01c60
NoClaus <support@marketer.consulting>
2022-03-08 13:00:44 +0000

    Added route for repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   scope module: :web do
     root 'welcome#index'
-    
+
     get '/auth/:provider/callback', to: 'auth#callback', as: :callback_auth, via: :all
     post '/auth/:provider', to: 'auth#request', as: :auth_request
     delete '/auth/logout', to: 'auth#logout', as: :auth_logout
+
+    resources :repositories
   end
 end
